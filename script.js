3ed1ae0a8790bba1d1d79801a7ef5c68ecfb3852
Dan Snow <danjsnow@outlook.com>
2023-03-21 16:14:19 +0000

    Change from console input to buttons with text output

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const result = document.querySelector('.result');
+
 function getComputerChoice() {
-  let random_number = Math.floor(Math.random() * 3);
-  switch (random_number) {
-    case 0:
-      return "rock";
-    case 1:
-      return "scissors";
-    case 2:
-      return "paper";
-    default:
-      return "error"; 
-  }
+    let random_number = Math.floor(Math.random() * 3);
+    switch (random_number) {
+      case 0:
+        return 'rock';
+      case 1:
+        return 'scissors';
+      case 2:
+        return 'paper';
+      default:
+        return 'error'; 
+    }
 }
 
 function judgeRound(playerSelection, computerSelection){
   switch (true) {
-    case playerSelection == computerSelection:
-      return "Draw - the computer also picked " + playerSelection;
-    case playerSelection == "rock" && computerSelection == "scissors":
-      return "You win! Rock beats scissors.";
-    case playerSelection == "scissors" && computerSelection == "paper":
-      return "You win! Scissors beats paper.";
-    case playerSelection == "paper" && computerSelection == "rock":
-      return "You win! Paper beats rock.";
-    case computerSelection == "rock" && playerSelection == "scissors":
-      return "You lose! Rock beats scissors.";
-    case computerSelection == "scissors" && playerSelection == "paper":
-      return "You lose! Scissors beats paper.";
-    case computerSelection == "paper" && playerSelection == "rock":
-      return "You lose! Paper beats rock.";
+    case playerSelection === computerSelection:
+      return 'Draw - the computer also picked ' + playerSelection;
+    case playerSelection === 'rock' && computerSelection == 'scissors':
+      return 'You win! Rock beats scissors.';
+    case playerSelection === 'scissors' && computerSelection == 'paper':
+      return 'You win! Scissors beats paper.';
+    case playerSelection === 'paper' && computerSelection == 'rock':
+      return 'You win! Paper beats rock.';
+    case computerSelection === 'rock' && playerSelection == 'scissors':
+      return 'You lose! Rock beats scissors.';
+    case computerSelection === 'scissors' && playerSelection == 'paper':
+      return 'You lose! Scissors beats paper.';
+    case computerSelection === 'paper' && playerSelection == 'rock':
+      return 'You lose! Paper beats rock.';
     default:
-      return "Hmm, doesn't look like you gave a valid answer." +
-             "Try typing 'rock', 'paper' or 'scissors' next time."
+      return 'Hmm, doesn\'t look like you gave a valid answer.' +
+             'Try typing \'rock\', \'paper\' or \'scissors\' next time.';
   }
 }
 
-function playRound() {
-  let playerSelection = prompt().toLowerCase().trim();
+function playRound(playerSelection) {
   let computerSelection = getComputerChoice();
   return judgeRound(playerSelection, computerSelection);
 }
 
-for (let i = 0; i < 5; i++) {
-  console.log(playRound());
-}
-
+// directly update the result text content after playing a round:
+rock.addEventListener('click', () => {
+  result.textContent = playRound("rock");
+})
+paper.addEventListener('click', () => {
+  result.textContent = playRound("paper");
+})
+scissors.addEventListener('click', () => {
+  result.textContent = playRound("scissors");
+})
